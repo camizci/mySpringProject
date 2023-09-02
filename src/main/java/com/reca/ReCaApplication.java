1f008c967e80f6363c923e21a1e9222b28f9d82c
Ramazan Camizci <camizci.r@gmail.com>
2023-09-03 00:44:33 +0200

    Spring Boot Demo -Review Topics

---
+++ src/main/java/com/reca/ReCaApplication.java
@@ -0,0 +1,13 @@
+package com.reca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReCaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReCaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reca/ReCaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reca;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReCaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
